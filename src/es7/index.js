ec849af5d06e5ea92fed9f11a9fd02fd39e46e95
SneiderSanchez <sneiderandres09@hotmail.com>
2020-12-02 18:08:08 -0500

    es7

---
+++ src/es7/index.js
@@ -0,0 +1,11 @@
+let numbers = [1, 2, 3, 7, 8];
+if (numbers.includes(9)) {
+  console.log("Numbers have 7 :)");
+} else {
+  console.log("that doesnt exists on numbers D:");
+}
+
+let base = 4;
+let exponent = 3;
+let result = base ** exponent;
+console.log(result);
