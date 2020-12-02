15f0f368ae2fda6a86f6d9e18c9c2d7258415226
SneiderSanchez <sneiderandres09@hotmail.com>
2020-12-02 17:20:21 -0500

    let,const,spread operator and destructuring

--- src/es6/index.js
+++ src/es6/index.js
@@ -19,5 +19,51 @@ let world = "world";
 let epicPhrase = hello + " " + world;
 console.log(epicPhrase);
 
+// es6
 let epicPhrase2 = `${hello} ${world}`;
 console.log(epicPhrase);
+
+let lorem =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit\n" + "Other stuff";
+
+// es6
+let lorem2 = `Other stuff
+Lorem ipsum dolor sit amet, consectetur adipiscing elit
+`;
+
+console.log(lorem);
+console.log(lorem2);
+
+let person = {
+  name: "Sneider",
+  age: 22,
+  country: "COL",
+};
+
+console.log(person.name, person.age, person.country);
+
+let { name, age, country } = person;
+
+console.log(name, age, country);
+let team1 = ["Sneider", "Andres", "Juan"];
+let team2 = ["Paola", "Vivian", "Anya"];
+
+let education = ["Pedro", ...team1, ...team2];
+
+console.log(education);
+
+{
+  var globalVar = "Global Var";
+}
+
+{
+  let globalLet = "Global let";
+  console.log(globalLet);
+}
+
+console.log(globalVar);
+
+const a = 'b';
+a = 'a'
+console.log(a);
+
