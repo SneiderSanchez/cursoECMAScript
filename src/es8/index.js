c8746733f5b31612a3687384864fcfee63d9095e
SneiderSanchez <sneiderandres09@hotmail.com>
2020-12-03 16:47:10 -0500

    Async Await

--- src/es8/index.js
+++ src/es8/index.js
@@ -20,3 +20,29 @@ console.log("food".padEnd(12, "----"));
 const obj = {
   name: "oscar",
 };
+
+const helloWorld = () => {
+  return new Promise((resolve, reject) => {
+    false
+      ? setTimeout(() => resolve("Hello world"), 3000)
+      : reject(new Error("test Error"));
+  });
+};
+
+const helloAsync = async () => {
+  const hello = await helloWorld();
+  console.log(hello);
+};
+
+helloAsync();
+
+const anotherFunction = async () => {
+  try {
+    const hello = await helloWorld();
+    console.log(hello);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+anotherFunction();
